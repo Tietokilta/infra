53f824cbca86563f716a4427f80c923dda5bb8af
Jussi Joki <judejoki@gmail.com>
2021-03-07 16:48:36 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,57 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "tikprodterraform"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+locals {
+  env_prefix = "prod"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tikweb_rg" {
+  name     = "tikWebResourceGroup"
+  location = "northeurope"
+}
+
+resource "azurerm_storage_account" "tikweb_sa" {
+  name                      = "${local.env_prefix}tikwebstorageaccount"
+  resource_group_name       = azurerm_resource_group.tikweb_rg.name
+  location                  = azurerm_resource_group.tikweb_rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = true
+  allow_blob_public_access  = true
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+}
+
+resource "azurerm_storage_container" "frontend_container" {
+  name                  = "${local.env_prefix}frontendcontainer"
+  storage_account_name  = azurerm_storage_account.tikweb_sa.name
+  container_access_type = "blob"
+}
+
+resource "azurerm_storage_blob" "index_html" {
+  name                    = "index.html"
+  storage_account_name    = azurerm_storage_account.tikweb_sa.name
+  storage_container_name  = azurerm_storage_container.frontend_container.name
+  type                    = "Block"
+  content_type            = "text/html"
+  source                  = "index.html"
+}
