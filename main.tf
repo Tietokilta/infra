c097441812d0685f581e696bf5b3ed04ef354689
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-05-08 16:33:42 +0300

    Restructure terraform

--- main.tf
+++ main.tf
@@ -11,45 +11,18 @@ terraform {
   }
 }
 
-locals {
-  env_prefix = "prod"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "tikweb_rg" {
-  name     = "tikWebResourceGroup"
-  location = "northeurope"
+module "common" {
+  source   = "./modules/common"
+  env_name = terraform.workspace
 }
 
-resource "azurerm_storage_account" "tikweb_sa" {
-  name                      = "${local.env_prefix}tikwebsa"
-  resource_group_name       = azurerm_resource_group.tikweb_rg.name
-  location                  = azurerm_resource_group.tikweb_rg.location
-  account_tier              = "Standard"
-  account_replication_type  = "GRS"
-  enable_https_traffic_only = true
-  allow_blob_public_access  = true
-
-  static_website {
-    index_document     = "index.html"
-    error_404_document = "404.html"
-  }
-}
-
-resource "azurerm_storage_container" "frontend_container" {
-  name                  = "${local.env_prefix}frontendcontainer"
-  storage_account_name  = azurerm_storage_account.tikweb_sa.name
-  container_access_type = "blob"
-}
-
-resource "azurerm_storage_blob" "index_html" {
-  name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.tikweb_sa.name
-  storage_container_name = azurerm_storage_container.frontend_container.name
-  type                   = "Block"
-  content_type           = "text/html"
-  source                 = "index.html"
+module "frontend" {
+  source                  = "./modules/frontend"
+  env_name                = terraform.workspace
+  resource_group_name     = module.common.resource_group_name
+  resource_group_location = module.common.resource_group_location
 }
