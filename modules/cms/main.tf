69f3af43466e2d147d33af09069187fbbb1a189b
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2021-05-23 13:18:01 +0300

    Fix collation

--- modules/cms/main.tf
+++ modules/cms/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_postgresql_database" "tikweb_cms_db" {
   resource_group_name = var.resource_group_name
   server_name         = var.postgres_server_name
   charset             = "UTF8"
-  collation           = "fi_FI"
+  collation           = "fi-FI"
 }
 
 
