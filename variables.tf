4b8e50c54590831c114e2e19df4bb316c4c4ee1e
Henri Tunkkari <henri.tunkkari@aalto.fi>
2022-01-04 11:25:10 +0200

    Add recruiting platform modules (#12)
    Add recruiting platform modules

--- variables.tf
+++ variables.tf
@@ -3,5 +3,15 @@ variable "postgres_admin_password" {
 }
 
 variable "strapi_admin_jwt_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "ghost_mail_username" {
   type = string
+}
+
+variable "ghost_mail_password" {
+  type      = string
+  sensitive = true
 }
