e0480573ae6a6b849d375eb3ca68ff0bbadf6260
Petrus Asikainen <petrus.asikainen@aalto.fi>
2023-02-11 00:06:35 +0200

    Fix ACME server URL

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ provider "dns" {
 }
 
 provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 locals {
