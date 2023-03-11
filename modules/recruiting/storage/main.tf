a44519a0c928c784734f019c11233ec4054cc392
Henri Tunkkari <henri.tunkkari@tietokilta.fi>
2023-03-11 23:06:13 +0200

    Remove old mysql instance

--- modules/recruiting/storage/main.tf
+++ modules/recruiting/storage/main.tf
@@ -9,35 +9,6 @@ resource "random_password" "db_password" {
   override_special = "_%@"
 }
 
-resource "azurerm_mysql_server" "tikjob_mysql" {
-  name                = "tikjob-${var.env_name}-mysql-server"
-  location            = azurerm_resource_group.tikjob_rg.location
-  resource_group_name = azurerm_resource_group.tikjob_rg.name
-
-  administrator_login          = var.ghost_db_username
-  administrator_login_password = random_password.db_password.result
-
-  sku_name   = "B_Gen5_1"
-  storage_mb = 5120 #Max size in MB
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-}
-
-resource "azurerm_mysql_database" "tikjob_mysql_db" {
-  name                = "tikjob_${var.env_name}_ghost"
-  resource_group_name = azurerm_resource_group.tikjob_rg.name
-  server_name         = azurerm_mysql_server.tikjob_mysql.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
-
 resource "azurerm_mysql_flexible_server" "tikjob_mysql_new" {
   name                   = "tikjob-${var.env_name}-mysql-flexible"
   resource_group_name    = azurerm_resource_group.tikjob_rg.name
@@ -55,15 +26,6 @@ resource "azurerm_mysql_flexible_database" "tikjob_mysql_db_new" {
   server_name         = azurerm_mysql_flexible_server.tikjob_mysql_new.name
   charset             = "utf8mb3"
   collation           = "utf8mb3_unicode_ci"
-}
-
-# Enable access from other Azure services (TODO: Switch to IP list)
-resource "azurerm_mysql_firewall_rule" "tikjob_mysql_access" {
-  name                = "tikjob-${var.env_name}-mysql-access"
-  resource_group_name = azurerm_resource_group.tikjob_rg.name
-  server_name         = azurerm_mysql_server.tikjob_mysql.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
 }
 
 # Enable access from other Azure services (TODO: Switch to IP list)
