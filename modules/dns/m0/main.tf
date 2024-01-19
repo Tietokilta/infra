e639527f908738643a8c20d3d1929c84b066cb9f
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-01-19 10:35:51 +0200

    point m0.fi dns to vercel

--- modules/dns/m0/main.tf
+++ modules/dns/m0/main.tf
@@ -8,11 +8,20 @@ resource "azurerm_dns_zone" "root_zone" {
   resource_group_name = "dns-m0-rg"
 }
 
+# A record for root
+resource "azurerm_dns_a_record" "root_a" {
+  name                = "@"
+  resource_group_name = azurerm_resource_group.dns_rg.name
+  zone_name           = azurerm_dns_zone.root_zone.name
+  ttl                 = 300
+  records             = ["76.76.21.21"]
+}
+
 # CNAME record for www.
 resource "azurerm_dns_cname_record" "www_cname" {
   name                = "www"
   resource_group_name = azurerm_resource_group.dns_rg.name
   zone_name           = azurerm_dns_zone.root_zone.name
   ttl                 = 300
-  record              = azurerm_dns_zone.root_zone.name
+  record              = "cname.vercel-dns.com."
 }
