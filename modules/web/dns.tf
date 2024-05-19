c129f95736ea6d4084e75b4333afd240f99aaf6c
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2024-05-19 22:04:38 +0300

    remove tikweb_dmarc

--- modules/web/dns.tf
+++ modules/web/dns.tf
@@ -46,18 +46,6 @@ resource "azurerm_dns_txt_record" "tikweb_asuid_www" {
   }
 }
 
-# Reporting-only DMARC policy
-resource "azurerm_dns_txt_record" "tikweb_dmarc" {
-  name                = "_dmarc${local.asuid_domain}"
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    value = "v=DMARC1;p=none;sp=none;rua=mailto:dmarc@tietokilta.fi!10m;ruf=mailto:dmarc@tietokilta.fi!10m"
-  }
-}
-
 # https://github.com/hashicorp/terraform-provider-azurerm/issues/14642#issuecomment-1084728235
 # Currently, the azurerm provider doesn't give us the IP address, so we need to fetch it ourselves.
 data "dns_a_record_set" "tikweb_dns_fetch" {
