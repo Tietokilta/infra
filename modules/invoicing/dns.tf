1a32672205fb3d5dfda1716a781d4f338b776871
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-08-11 19:05:54 +0300

    feat: Deploy invoice generator

--- modules/invoicing/dns.tf
+++ modules/invoicing/dns.tf
@@ -9,6 +9,7 @@ resource "azurerm_dns_mx_record" "invoicing_mx" {
     preference = 10
     exchange   = "mxa.eu.mailgun.org"
   }
+
   record {
     preference = 10
     exchange   = "mxb.eu.mailgun.org"
@@ -48,5 +49,26 @@ resource "azurerm_dns_txt_record" "invoicing_dmarc" {
 
   record {
     value = "v=DMARC1;p=none;sp=none;rua=mailto:dmarc@tietokilta.fi!10m;ruf=mailto:dmarc@tietokilta.fi!10m"
+  }
+}
+
+# A record for the web app
+resource "azurerm_dns_a_record" "invoice_generator_a" {
+  name                = var.subdomain
+  resource_group_name = var.dns_resource_group_name
+  zone_name           = var.root_zone_name
+  ttl                 = 300
+  records             = data.dns_a_record_set.invoice_generator_dns_fetch.addrs
+}
+
+# Azure verification key
+resource "azurerm_dns_txt_record" "invoice_generator_asuid" {
+  name                = "asuid.${var.subdomain}"
+  resource_group_name = var.dns_resource_group_name
+  zone_name           = var.root_zone_name
+  ttl                 = 300
+
+  record {
+    value = azurerm_linux_web_app.invoice_generator.custom_domain_verification_id
   }
 }
--- modules/invoicing/variables.tf
+++ modules/invoicing/variables.tf
@@ -1,4 +1,29 @@
-variable "env_name" {
+variable "mailgun_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "mailgun_url" {
+  type = string
+}
+
+variable "mailgun_user" {
+  type = string
+}
+
+variable "app_service_plan_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "subdomain" {
   type = string
 }
 
@@ -10,7 +35,7 @@ variable "root_zone_name" {
   type = string
 }
 
-variable "subdomain" {
+variable "acme_account_key" {
   type = string
 }
 
