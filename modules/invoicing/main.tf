935e1edbde40a7ad4e2d2239a168bf9f0a2cd589
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-10-31 17:51:13 +0200

    feat: Remove lifecycle ignore changes for invoice generator docker image name

--- modules/invoicing/main.tf
+++ modules/invoicing/main.tf
@@ -24,12 +24,6 @@ resource "azurerm_linux_web_app" "invoice_generator" {
 
     http2_enabled = true
   }
-  lifecycle {
-    // image is deployed by web-repos GHA workflow
-    ignore_changes = [
-      site_config.0.application_stack.0.docker_image_name,
-    ]
-  }
   logs {
     http_logs {
       file_system {
