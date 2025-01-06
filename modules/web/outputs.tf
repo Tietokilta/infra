6e526e00b9a7b4df0befc237fce965cbedcf9cf1
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-01-06 10:22:56 +0200

    chore: upgrade azuread, move keyvault secret names outside of module

--- modules/web/outputs.tf
+++ modules/web/outputs.tf
@@ -5,3 +5,9 @@ output "payload_password" {
   value     = random_password.payload_password.result
   sensitive = true
 }
+output "web_app_id" {
+  value = azurerm_linux_web_app.web.id
+}
+output "cms_app_id" {
+  value = azurerm_linux_web_app.cms.id
+}
