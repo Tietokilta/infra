bf0e0018d4c8e5d1258b9b820916f883194dd226
IiroP <iiro.poutiainen@gmail.com>
2025-03-30 12:30:23 +0300

    feat: Create oldweb app to Azure

--- modules/oldweb/main.tf
+++ modules/oldweb/main.tf
@@ -38,4 +38,63 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
+  admin_enabled       = false
+}
+
+# Create actual application
+resource "azurerm_linux_web_app" "oldweb_backend" {
+  name                = "tik-oldweb-${var.env_name}-app"
+  location            = var.tikweb_rg_location
+  resource_group_name = var.tikweb_rg_name
+  service_plan_id     = var.tikweb_app_plan_id
+
+  https_only = true
+
+  site_config {
+    ftps_state = "Disabled"
+    always_on  = true
+
+    application_stack {
+      docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
+      docker_image_name   = "oldweb:latest"
+    }
+  }
+
+  logs {
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 100
+      }
+    }
+  }
+
+  # Mount the Azure File Share for images and other attachments
+  storage_account {
+    name         = "oldweb-persistent-storage"
+    account_name = azurerm_storage_account.storage_account.name
+    access_key   = azurerm_storage_account.storage_account.primary_access_key
+    mount_path   = "/app/public/page_attachments"
+    share_name   = azurerm_storage_share.file_share.name
+    type         = "AzureFiles"
+  }
+
+  # Configuration for oldweb
+  app_settings = {
+    WEBSITES_PORT = 3000
+    PORT          = 3000
+
+    DB_USERNAME = "tietokilta"
+    DB_PASSWORD = var.postgres_admin_password
+    DB_HOST     = var.postgres_server_fqdn
+    DB_DATABASE = local.db_name
+    DB_PORT     = 5432
+
+  }
+
+  lifecycle {
+    ignore_changes = [
+      site_config.0.application_stack, # deployments are made outside of Terraform
+    ]
+  }
 }
