3adfba69a4108fda15c94dddd246eac0015e68b2
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-07-06 22:02:08 +0300

    chore: refactor status to use app_service_hostname

--- modules/status/main.tf
+++ modules/status/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    acme = {
-      source = "vancluever/acme"
-    }
-  }
-}
-
-locals {
-  fqdn = "${var.subdomain}.${var.root_zone_name}"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "status-rg"
   location = var.location
@@ -48,78 +36,16 @@ resource "azurerm_linux_web_app" "status_app" {
 }
 
 
-# A record for the web app
-resource "azurerm_dns_a_record" "status_a" {
-  name                = var.subdomain
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-  records             = data.dns_a_record_set.status_dns_fetch.addrs
-}
-
-# Azure verification key
-resource "azurerm_dns_txt_record" "status_asuid" {
-  name                = "asuid.${var.subdomain}"
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    value = azurerm_linux_web_app.status_app.custom_domain_verification_id
-  }
-}
-
-
-resource "azurerm_app_service_custom_hostname_binding" "status_hostname_binding" {
-  hostname            = local.fqdn
-  app_service_name    = azurerm_linux_web_app.status_app.name
-  resource_group_name = var.app_service_plan_resource_group_name
-
-  # Deletion may need manual work.
-  # https://github.com/hashicorp/terraform-provider-azurerm/issues/11231
-  # TODO: Add dependencies for creation
-  depends_on = [
-    azurerm_dns_a_record.status_a,
-    azurerm_dns_txt_record.status_asuid
-  ]
-}
-
-resource "random_password" "status_cert_password" {
-  length  = 48
-  special = false
-}
-
-resource "acme_certificate" "status_acme_cert" {
-  account_key_pem          = var.acme_account_key
-  common_name              = local.fqdn
-  key_type                 = "2048" # RSA
-  certificate_p12_password = random_password.status_cert_password.result
-
-  dns_challenge {
-    provider = "azuredns"
-    config = {
-      AZURE_RESOURCE_GROUP = var.dns_resource_group_name
-      AZURE_ZONE_NAME      = var.root_zone_name
-    }
-  }
-}
-
-resource "azurerm_app_service_certificate" "status_cert" {
-  name                = "tik-status-cert-${terraform.workspace}"
-  resource_group_name = var.app_service_plan_resource_group_name
-  location            = var.location
-  pfx_blob            = acme_certificate.status_acme_cert.certificate_p12
-  password            = acme_certificate.status_acme_cert.certificate_p12_password
-}
-
-resource "azurerm_app_service_certificate_binding" "status_cert_binding" {
-  certificate_id      = azurerm_app_service_certificate.status_cert.id
-  hostname_binding_id = azurerm_app_service_custom_hostname_binding.status_hostname_binding.id
-  ssl_state           = "SniEnabled"
-}
-
-# https://github.com/hashicorp/terraform-provider-azurerm/issues/14642#issuecomment-1084728235
-# Currently, the azurerm provider doesn't give us the IP address, so we need to fetch it ourselves.
-data "dns_a_record_set" "status_dns_fetch" {
-  host = azurerm_linux_web_app.status_app.default_hostname
+module "app_service_hostname" {
+  source                          = "../app_service_hostname"
+  subdomain                       = var.subdomain
+  root_zone_name                  = var.root_zone_name
+  dns_resource_group_name         = var.dns_resource_group_name
+  custom_domain_verification_id   = azurerm_linux_web_app.status_app.custom_domain_verification_id
+  app_service_name                = azurerm_linux_web_app.status_app.name
+  app_service_resource_group_name = var.app_service_plan_resource_group_name
+  app_service_location            = var.location
+  app_service_default_hostname    = azurerm_linux_web_app.status_app.default_hostname
+  acme_account_key                = var.acme_account_key
+  certificate_name                = "tik-status-cert-${terraform.workspace}"
 }
