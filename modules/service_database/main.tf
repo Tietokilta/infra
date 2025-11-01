ae1315a9ac91cba8339d940518b977f8b3bdaf45
IiroP <iiro.poutiainen@gmail.com>
2025-11-01 15:06:47 +0200

    fix: Grant db user permissions to all existing and future tables

--- modules/service_database/main.tf
+++ modules/service_database/main.tf
@@ -12,7 +12,7 @@ resource "random_password" "db_user_password" {
 }
 
 
-# Create a database role scoped to the oldweb database
+# Create a database role scoped to the this database
 resource "postgresql_role" "db_user" {
   provider = postgresql.admin
   name     = "${var.db_name}_user"
@@ -20,11 +20,33 @@ resource "postgresql_role" "db_user" {
   password = random_password.db_user_password.result
 }
 
-# Grant the role privileges only on the oldweb database
+# Grant the role privileges only on this database
 resource "postgresql_grant" "db_role" {
   provider    = postgresql.admin
   role        = postgresql_role.db_user.name
   database    = azurerm_postgresql_flexible_server_database.database.name
   privileges  = ["ALL"]
   object_type = "database"
+}
+
+# Grant access to existing tables
+resource "postgresql_grant" "tables_access" {
+  provider    = postgresql.admin
+  database    = azurerm_postgresql_flexible_server_database.database.name
+  schema      = "public"
+  object_type = "table"
+  objects     = ["*"]
+  privileges  = ["ALL"]
+  role        = postgresql_role.db_user.name
+}
+
+# Default access to future tables
+resource "postgresql_default_privileges" "tables_access_future" {
+  provider    = postgresql.admin
+  database    = azurerm_postgresql_flexible_server_database.database.name
+  schema      = "public"
+  object_type = "table"
+  privileges  = ["ALL"]
+  role        = postgresql_role.db_user.name
+  owner       = postgresql_role.db_user.name
 }
