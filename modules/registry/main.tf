4634e2f0400ec8c2f7da51cce182351b49454cbd
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2025-11-01 15:19:32 +0200

    feat: add registry

---
+++ modules/registry/main.tf
@@ -0,0 +1,77 @@
+locals {
+  postgres_db_name = "registry_${var.environment}"
+}
+
+# Create postgres database
+module "service_database" {
+  source = "../service_database"
+
+  db_name                 = local.postgres_db_name
+  postgres_server_id      = var.postgres_server_id
+  postgres_admin_username = var.postgres_admin_username
+  postgres_admin_password = var.postgres_admin_password
+  postgres_server_fqdn    = var.postgres_server_fqdn
+}
+
+resource "azurerm_linux_web_app" "registry" {
+  name                = "tik-registry-${var.environment}"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = var.app_service_plan_id
+
+  site_config {
+    application_stack {
+      docker_registry_url = "https://ghcr.io"
+      docker_image_name   = "tietokilta/rekisteri:latest"
+    }
+
+    http2_enabled = true
+  }
+
+  logs {
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 100
+      }
+    }
+  }
+
+  https_only = true
+  app_settings = {
+    NODE_ENV = "production"
+
+    DB_URL      = var.postgres_server_fqdn
+    DB_USER     = module.service_database.db_user
+    DB_PASSWORD = module.service_database.db_password
+    DB_DATABASE = module.service_database.db_name
+
+    MAILGUN_SENDER  = "noreply@${var.mailgun_domain}"
+    MAILGUN_API_KEY = var.mailgun_api_key
+    MAILGUN_DOMAIN  = var.mailgun_domain
+    MAILGUN_URL     = var.mailgun_url
+
+    STRIPE_API_KEY        = var.stripe_api_key
+    STRIPE_WEBHOOK_SECRET = var.stripe_webhook_secret
+  }
+
+  lifecycle {
+    ignore_changes = [
+      site_config.0.application_stack, # deployments are made outside of Terraform
+    ]
+  }
+}
+
+module "app_service_hostname" {
+  source                          = "../app_service_hostname"
+  subdomain                       = var.subdomain
+  root_zone_name                  = var.root_zone_name
+  dns_resource_group_name         = var.dns_resource_group_name
+  custom_domain_verification_id   = azurerm_linux_web_app.registry.custom_domain_verification_id
+  app_service_name                = azurerm_linux_web_app.registry.name
+  app_service_resource_group_name = var.resource_group_name
+  app_service_location            = var.resource_group_location
+  app_service_default_hostname    = azurerm_linux_web_app.registry.default_hostname
+  acme_account_key                = var.acme_account_key
+  certificate_name                = "tik-registry-cert-${var.environment}"
+}
--- modules/registry/variables.tf
+++ modules/registry/variables.tf
@@ -1,4 +1,37 @@
+variable "postgres_server_id" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "app_service_plan_id" {
+  type = string
+}
+
+variable "postgres_server_fqdn" {
+  type = string
+}
+
+variable "postgres_admin_username" {
+  type = string
+}
+
+variable "postgres_admin_password" {
+  type      = string
+  sensitive = true
+}
+
 variable "subdomain" {
+  type = string
+}
+
+variable "root_zone_name" {
   type = string
 }
 
@@ -6,8 +39,35 @@ variable "dns_resource_group_name" {
   type = string
 }
 
-variable "root_zone_name" {
+variable "acme_account_key" {
   type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "mailgun_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "mailgun_domain" {
+  type = string
+}
+
+variable "mailgun_url" {
+  type = string
+}
+
+variable "stripe_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "stripe_webhook_secret" {
+  type      = string
+  sensitive = true
 }
 
 variable "dkim_selector" {
