4d5a6fa85bdd6f7f9a570df89d9339d127611c48
langsjo <104687438+langsjo@users.noreply.github.com>
2025-11-02 18:20:45 +0200

    feat: add dns for vaalit.staging.tietokilta.fi and pannu.tietokilta.fi

---
+++ modules/tikpannu/variables.tf
@@ -0,0 +1,15 @@
+variable "dns_resource_group_name" {
+  type = string
+}
+
+variable "root_zone_name" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+}
+
+variable "tikpannu_ip" {
+  type = string
+}
