017e9e163730462d94aefa43a92fe21a12adca05
langsjo <104687438+langsjo@users.noreply.github.com>
2025-12-16 20:20:45 +0200

    docs(pannu): add documentation for the tikpannu nixos config

--- tikpannu-nixos-config/configuration.nix
+++ tikpannu-nixos-config/configuration.nix
@@ -41,5 +41,10 @@
     ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiDxF/PZcYfX6N3CQOdQdW0PTPN7tgmwL6RPFDBlJURsxiTlmlRygjMVjrnxbIN9KGIP2p3hUKxensm0ftbl3fvdBG3nUnreGZAUQ7prSrli3tv+WITPFdONtDqcrMlYXbBy51/kFLUQMV7wBYurM/4bW/BOXtNZdk8/dLyCqAr1ynZmXFFHEB3APtlxaLlsyHEER5Nj7WDlxpFUxOqzasPg8MMGKQeN+d2TbUq1s0YDVwmk4F+Zqfj0H9AAYYt4zkiKbCkzTrJXk9snBPAyUot8jkAjZW5nu7quVoiHvWY3335iaa4o2JWDkm6/QEXYzKIbi865jOr3A5DRFytNFQJ7nmXfSNWAJmblSlatlszQLwmTLP5wkV+3zbRHv7WuvWivR76Xy0uyK331UvqrRbNha+EbVoWP5DyFnichBH7B/IgHkLHQJIuYiQBZ2ZwTuVpEoxyCUyl9acDtmUZvuomTAEjLRQElnhRo8iyDf92dl19Q9dG/1RWqLXUEDVBcLrlk89aEnIk7DuwvmVWzWM+On9S8ojH04TgRJM5ZkbQLAIqW5AkLqY6CP5Gzknsh7F4fl5Mq0FZlCOtFzxR+YgIn4IGndonm8/iqDQjJNOWVysFdNRPisPSR5AO5TiuxZSOcCuRkS56cZTHKjdqZS8CxiCfs2ZPlzMnzKJSNDXxQ==''
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIhL1onG+Ikt+kBeGon6qIDe8ay/31a8dc+DbOB4sV+a"
   ];
-  system.stateVersion = "23.11";
+
+  # DO NOT TOUCH THIS
+  # Changing this variable does not update the system
+  # and may corrupt the databases running on it
+  system.stateVersion = "23.11"; # DO NOT TOUCH THIS
+  # DO NOT TOUCH THIS
 }
