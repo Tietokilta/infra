e81d32475731a0f24120178f8d4b405662301d1f
langsjo <104687438+langsjo@users.noreply.github.com>
2025-12-17 18:02:35 +0200

    chore: use opentofu instead of terraform in flake

--- .pre-commit-hook.sh
+++ .pre-commit-hook.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env -S bash -euo pipefail
 
+if command -v tofu > /dev/null ; then
+    tf_cmd=tofu
+elif command -v terraform > /dev/null ; then
+    tf_cmd=terraform
+fi
+
 if command -v nix > /dev/null ; then
     nix fmt -- --fail-on-change
-else
-    if ! terraform fmt --recursive --check > /dev/null ; then
+elif [ -n "$tf_cmd" ] ; then
+    if ! "$tf_cmd" fmt --recursive --check > /dev/null ; then
         echo "The following terraform files have changed:" >&2
-        terraform fmt --recursive
+        "$tf_cmd" fmt --recursive
         exit 1
     fi
+else
+    echo "No nix, terraform, or tofu found to run formatters" >&2
+    exit 1
 fi
