13065f905808cada87c8348afd7f9a21199a9e7a
langsjo <104687438+langsjo@users.noreply.github.com>
2026-01-13 21:30:05 +0200

    fix: delete records from forum/dns.tf

--- modules/forum/dns.tf
+++ modules/forum/dns.tf
@@ -1,61 +1,61 @@
 # A record for Discourse
-resource "azurerm_dns_a_record" "forum_a" {
+resource "azurerm_dns_a_record" "forum_old_a" {
   name                = var.subdomain
   resource_group_name = var.dns_resource_group_name
   zone_name           = var.root_zone_name
   ttl                 = 300
   records             = [azurerm_public_ip.forum_ip.ip_address]
 }
-
-# MX records for Mailgun
-resource "azurerm_dns_mx_record" "forum_mx" {
-  name                = var.subdomain
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    preference = 10
-    exchange   = "mxa.eu.mailgun.org"
-  }
-  record {
-    preference = 10
-    exchange   = "mxb.eu.mailgun.org"
-  }
-}
-
-# SPF record for Mailgun
-resource "azurerm_dns_txt_record" "forum_spf" {
-  name                = var.subdomain
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    value = "v=spf1 include:mailgun.org ~all"
-  }
-}
-
-# DKIM key for Mailgun
-resource "azurerm_dns_txt_record" "forum_dkim" {
-  name                = "${var.dkim_selector}._domainkey.${var.subdomain}"
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    value = var.dkim_key
-  }
-}
-
-# Reporting-only DMARC policy
-resource "azurerm_dns_txt_record" "forum_dmarc" {
-  name                = "_dmarc.${var.subdomain}"
-  resource_group_name = var.dns_resource_group_name
-  zone_name           = var.root_zone_name
-  ttl                 = 300
-
-  record {
-    value = "v=DMARC1;p=none;sp=none;rua=mailto:dmarc@tietokilta.fi!10m;ruf=mailto:dmarc@tietokilta.fi!10m"
-  }
-}
+#
+# # MX records for Mailgun
+# resource "azurerm_dns_mx_record" "forum_mx" {
+#   name                = var.subdomain
+#   resource_group_name = var.dns_resource_group_name
+#   zone_name           = var.root_zone_name
+#   ttl                 = 300
+#
+#   record {
+#     preference = 10
+#     exchange   = "mxa.eu.mailgun.org"
+#   }
+#   record {
+#     preference = 10
+#     exchange   = "mxb.eu.mailgun.org"
+#   }
+# }
+#
+# # SPF record for Mailgun
+# resource "azurerm_dns_txt_record" "forum_spf" {
+#   name                = var.subdomain
+#   resource_group_name = var.dns_resource_group_name
+#   zone_name           = var.root_zone_name
+#   ttl                 = 300
+#
+#   record {
+#     value = "v=spf1 include:mailgun.org ~all"
+#   }
+# }
+#
+# # DKIM key for Mailgun
+# resource "azurerm_dns_txt_record" "forum_dkim" {
+#   name                = "${var.dkim_selector}._domainkey.${var.subdomain}"
+#   resource_group_name = var.dns_resource_group_name
+#   zone_name           = var.root_zone_name
+#   ttl                 = 300
+#
+#   record {
+#     value = var.dkim_key
+#   }
+# }
+#
+# # Reporting-only DMARC policy
+# resource "azurerm_dns_txt_record" "forum_dmarc" {
+#   name                = "_dmarc.${var.subdomain}"
+#   resource_group_name = var.dns_resource_group_name
+#   zone_name           = var.root_zone_name
+#   ttl                 = 300
+#
+#   record {
+#     value = "v=DMARC1;p=none;sp=none;rua=mailto:dmarc@tietokilta.fi!10m;ruf=mailto:dmarc@tietokilta.fi!10m"
+#   }
+# }
