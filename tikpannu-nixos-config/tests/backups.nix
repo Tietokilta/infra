e56055ab808d6c67529762595ae2c8cc1b3a60e6
langsjo <104687438+langsjo@users.noreply.github.com>
2026-02-27 15:04:54 +0200

    feat: init backup tests, slight refactor

---
+++ tikpannu-nixos-config/tests/backups.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  name = "restic-backups";
+
+  nodes.pannu = {
+    imports = [
+      ./base-pannu-config.nix
+    ];
+
+    services.tik-backup.enable = lib.mkForce true;
+
+    # Enable discourse for the backup logic, not to run it
+    services.discourse.enable = lib.mkForce true;
+    systemd.services.discourse.wantedBy = lib.mkForce [ ];
+
+    systemd.tmpfiles.rules = [
+      "d /mnt/backup 0700 backup backup -"
+      "d /var/lib/discourse/backups/default 0700 discourse discourse -"
+    ];
+
+    environment.systemPackages = [
+      pkgs.jq
+    ];
+  };
+
+  testScript = /* python */ ''
+    def unit_succeeded(unit_name: str):
+      # Check that the unit actually ran
+      _, exit_time = pannu.execute(
+        f"systemctl show -p ExecMainExitTimestamp --value '{unit_name}'"
+      )
+      _, result = pannu.execute(
+        f"systemctl show -p Result --value '{unit_name}'"
+      )
+      assert exit_time.strip() != "", f"Unit {unit_name} never ran"
+      assert result.strip() == "success", f"Unit {unit_name} did not succeed, got {result}"
+
+    start_all()
+
+    # Discourse staging service only uses files that have been modified more
+    # than some minutes ago.
+    discourse_dummy = "/var/lib/discourse/backups/default/dummy_backup.tar.gz"
+    pannu.succeed(f''''
+      touch -m -d @0 {discourse_dummy} && \\
+        chown discourse:discourse {discourse_dummy}
+    '''')
+
+    pannu.succeed("systemctl start restic-backups-tik-backup.service")
+    unit_succeeded("discourse-stage-backup.service")
+
+    _, stdout = pannu.execute("restic-tik-backup ls latest")
+    print("Backed up files:")
+    print(stdout)
+
+    # At least min_files files must exist in the backup. This does not include
+    # directories.
+    min_files = 1
+    stdout = pannu.succeed(f''''
+      restic-tik-backup ls --json latest \\
+      | jq --exit-status --slurp \\
+        '[.[] | select(.type=="file")] | length | select(. >= {min_files})'
+    '''')
+    print(f"Found {stdout.strip()} files")
+  '';
+}
