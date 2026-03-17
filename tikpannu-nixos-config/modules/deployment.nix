ba30bfb62054b5f53a67ba66f58db1bb69a8d5f0
langsjo <104687438+langsjo@users.noreply.github.com>
2026-03-17 21:18:39 +0200

    fix: remove passing github_sha to rebuild-from-infra
    it never worked anyway (:D), also it is better to just rebuild from
    the default branch. github will have commit revs from forks reachable
    via the original repo, so this could allow rebuilding from any config

--- tikpannu-nixos-config/modules/deployment.nix
+++ tikpannu-nixos-config/modules/deployment.nix
@@ -6,7 +6,7 @@
 }:
 let
   rebuild-from-infra = pkgs.writeShellScriptBin "rebuild-from-infra" ''
-    ${lib.getExe config.system.build.nixos-rebuild} switch --flake github:Tietokilta/infra/"$1" --refresh
+    ${lib.getExe config.system.build.nixos-rebuild} switch --flake github:Tietokilta/infra --refresh
   '';
 in
 {
