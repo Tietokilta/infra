e10f11846598f796d68df552daab85dc3467c700
Luukas Pörtfors <lajp@iki.fi>
2026-04-21 17:16:40 +0300

    chore: migrate histotik to github pages

--- modules/histotik/dns.tf
+++ modules/histotik/dns.tf
@@ -3,8 +3,5 @@ resource "azurerm_dns_cname_record" "histotik_cname_record" {
   resource_group_name = var.dns_resource_group_name
   zone_name           = var.root_zone_name
   ttl                 = 300
-  record              = "${azurerm_cdn_endpoint.histotik_cdn_endpoint.name}.azureedge.net"
-  # TODO: use .fqdn when azurerm provider has been upgraded
+  record              = "tietokilta.github.io"
 }
-
-# TODO: add histo.tik.tietokilta.fi
--- modules/histotik/main.tf
+++ modules/histotik/main.tf
@@ -1,7 +1,3 @@
-locals {
-  fqdn = "${var.subdomain}.${var.root_zone_name}"
-}
-
 resource "azurerm_resource_group" "histotik_rg" {
   name     = "histotik-${var.env_name}-rg"
   location = var.resource_group_location
@@ -30,55 +26,4 @@ resource "azurerm_storage_container" "histotik_storage_container" {
   name                  = "$web"
   storage_account_id    = azurerm_storage_account.histotik_storage_account.id
   container_access_type = "blob"
-}
-
-resource "azurerm_cdn_profile" "histotik_cdn_profile" {
-  name                = "histotik-${var.env_name}-cdn"
-  resource_group_name = azurerm_resource_group.histotik_rg.name
-  location            = azurerm_resource_group.histotik_rg.location
-  sku                 = "Standard_Microsoft"
-}
-
-resource "azurerm_cdn_endpoint" "histotik_cdn_endpoint" {
-  name                = "histotik-${var.env_name}"
-  resource_group_name = azurerm_resource_group.histotik_rg.name
-  location            = azurerm_resource_group.histotik_rg.location
-  profile_name        = azurerm_cdn_profile.histotik_cdn_profile.name
-  origin_host_header  = azurerm_storage_account.histotik_storage_account.primary_web_host
-
-  origin {
-    name      = "storage"
-    host_name = azurerm_storage_account.histotik_storage_account.primary_web_host
-  }
-
-  delivery_rule {
-    name  = "httpsredirect"
-    order = 1
-
-    request_scheme_condition {
-      match_values = ["HTTP"]
-    }
-    url_redirect_action {
-      redirect_type = "Found"
-      protocol      = "Https"
-    }
-  }
-}
-
-resource "azurerm_cdn_endpoint_custom_domain" "histotik_cdn_domain" {
-  name            = "histotik-${var.env_name}-domain"
-  cdn_endpoint_id = azurerm_cdn_endpoint.histotik_cdn_endpoint.id
-  host_name       = local.fqdn
-
-  cdn_managed_https {
-    certificate_type = "Dedicated"
-    protocol_type    = "ServerNameIndication"
-    tls_version      = "TLS12"
-  }
-
-  # Deletion needs manual work. Hashicorp seems uninterested in fixing.
-  # https://github.com/hashicorp/terraform-provider-azurerm/issues/11231
-  depends_on = [
-    azurerm_dns_cname_record.histotik_cname_record
-  ]
 }
