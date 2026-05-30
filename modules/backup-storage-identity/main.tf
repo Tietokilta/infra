5f8489603f7430293240d8071b1a7d37e45b4433
langsjo <104687438+langsjo@users.noreply.github.com>
2026-05-30 17:45:24 +0300

    fix(backups): try to snapshot file share first before syncing
    Fixes syncing file share that is actively being written to.
    Also better logging

--- modules/backup-storage-identity/main.tf
+++ modules/backup-storage-identity/main.tf
@@ -34,3 +34,24 @@ resource "azurerm_role_assignment" "backup_storage_file_reader" {
   role_definition_name = "Storage File Data Privileged Reader"
   principal_id         = azuread_service_principal.backup_storage.object_id
 }
+
+resource "azurerm_role_definition" "file_share_snapshot" {
+  name        = var.snapshot_role_name
+  scope       = local.subscription_scope
+  description = "Modify and delete file shares, needed to create and delete snapshots"
+
+  permissions {
+    actions = [
+      "Microsoft.Storage/storageAccounts/fileServices/shares/write",
+      "Microsoft.Storage/storageAccounts/fileServices/shares/delete",
+    ]
+  }
+
+  assignable_scopes = [local.subscription_scope]
+}
+
+resource "azurerm_role_assignment" "backup_storage_file_snapshot" {
+  scope              = local.subscription_scope
+  role_definition_id = azurerm_role_definition.file_share_snapshot.role_definition_resource_id
+  principal_id       = azuread_service_principal.backup_storage.object_id
+}
