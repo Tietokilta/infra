8e7e165ff00501e95f2edf3b02f9bfe357bce76d
Mikael Siidorow <mikael@siidorow.com>
2026-07-23 17:35:46 +0300

    feat: pre-stage registry SMTP configuration

--- modules/registry/mailgun.tf
+++ modules/registry/mailgun.tf
@@ -4,6 +4,7 @@ module "mailgun" {
   domain_name            = "${var.subdomain}.${var.root_zone_name}"
   subdomain              = var.subdomain
   dns_zone_name          = var.root_zone_name
-  create_smtp_credential = false
+  create_smtp_credential = true
+  smtp_login             = "rekisteri-app"
   cloudflare_zone_id     = var.cloudflare_zone_id
 }
