dceba10f8f4aa969f43d088cb8da3a3c9e95024a
Joona Kärkkäinen <joona.karkkainen@gmail.com>
2026-09-22 14:25:43 +0300

    chore: dns records for analytics

--- modules/tikpannu/dns.tf
+++ modules/tikpannu/dns.tf
@@ -1,8 +1,16 @@
 resource "cloudflare_dns_record" "tikpannu_a" {
+  for_each = var.subdomains
+
   zone_id = var.cloudflare_zone_id
-  name    = var.subdomain
+  name    = each.value
   type    = "A"
   content = var.tikpannu_ip
   proxied = false
   ttl     = 300
+}
+
+# Retain the record created before this module took a set of subdomains
+moved {
+  from = cloudflare_dns_record.tikpannu_a
+  to   = cloudflare_dns_record.tikpannu_a["pannu"]
 }
